e4519b169f151abfce8d1d20ce962ab3f94e8ef9
Angel <angelSistemas21@gmail.com>
2022-04-29 08:07:34 -0500

    update: Archivo index.js - Instanciación en 3 objetos

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const MissionCommander = require('./app/missionCommander');
 
 const carlo = new MissionCommander("Carlo")
+const fernanda = new MissionCommander("Fernanda")
+const oscar = new MissionCommander("Oscar")
+
 console.log(carlo.name)
+console.log(fernanda.name)
+console.log(oscar.name)
